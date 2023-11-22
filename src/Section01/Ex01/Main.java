85a3b76383273b9e0829f20d6da0612ee93d21ec
raccoon0906 <lakun7157@gmail.com>
2023-11-23 04:29:53 +0900

    시작 알림메세지 출력

--- src/Section01/Ex01/Main.java
+++ src/Section01/Ex01/Main.java
@@ -2,6 +2,9 @@ package Section01.Ex01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("==자바 텍스트 게시판==");
+        System.out.println("==프로그램 시작==");
+        System.out.println("명령) ");
+        System.out.println("==프로그램 종료==");
     }
 }
