59d4c68afdb1236a06b7d537a582a5f267e4adb1
raccoon0906 <lakun7157@gmail.com>
2023-11-24 04:18:25 +0900

    게시물 등록

--- src/Section01/Ex01/Main.java
+++ src/Section01/Ex01/Main.java
@@ -9,13 +9,30 @@ public class Main {
 
         System.out.println("==자바 텍스트 게시판==");
         System.out.println("==프로그램 시작==");
+        int postNum = 1;
         while(true){
             System.out.print("명령) ");
             String cmd = sc.nextLine();
-            System.out.printf("입력된 명령어 : %s\n",cmd);
-            if (cmd.equals("exit")){
+
+            if (cmd.equals("usr/article/write")){
+                System.out.printf("입력된 명령어 : %s\n",cmd);
+                System.out.println("==게시물 등록==");
+
+                System.out.print("제목: ");
+                String title = sc.nextLine();
+                System.out.print("내용: ");
+                String detail = sc.nextLine();
+
+                System.out.printf("%d번 게시물이 등록되었습니다.\n",postNum);
+                postNum++;
+            }
+            else if (cmd.equals("exit")){
+                System.out.printf("입력된 명령어 : %s\n",cmd);
                 System.out.println("프로그램을 종료합니다");
                 break;
+            }
+            else {
+                System.out.println("명령어를 잘못 입력하셨습니다");
             }
         }
 
